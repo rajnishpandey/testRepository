20c7328a91f11ce0d705f198d2c2763af0b5b78d
rajnishkumar.pandey@cognizant.com <rajnishkumar.pandey@cognizant.com>
2016-06-29 12:21:02 -0400

    Initial check-in for Sample Template code

---
+++ reference6/src/main/java/com/staterkit/domain/Album.java
@@ -0,0 +1,98 @@
+package com.staterkit.domain;
+
+import org.springframework.data.annotation.Id;
+
+public class Album
+{
+    @Id
+    private Long id;
+
+    
+    private String title;
+    private String artist;
+    private String releaseYear;
+
+    public Album() {
+    }
+
+    /**
+     * @param title
+     * @param artist
+     * @param releaseYear
+     */
+    public Album(String title, String artist, String releaseYear) {
+        this.title = title;
+        this.artist = artist;
+        this.releaseYear = releaseYear;
+    }
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return tittle
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return artist
+     */
+    public String getArtist() {
+        return artist;
+    }
+
+    /**
+     * @param artist
+     */
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    /**
+     * @return release year
+     */
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    /**
+     * @param releaseYear
+     */
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Album{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", releaseYear='" + releaseYear + '\'' +
+                '}';
+    }
+}
